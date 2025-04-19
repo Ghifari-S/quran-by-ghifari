6a1bb35cef2be20738509550d808e5f668b48ba2
Ghifari-s <ghifaripooh@gmail.com>
2025-04-19 13:26:09 +0700

    first commit

---
+++ src/app/daftar-surat/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+interface Surah {
+  nomor: number;
+  nama: string;
+  nama_latin: string;
+  jumlah_ayat: number;
+  tempat_turun: string;
+  arti: string;
+  deskripsi: string;
+  audio: string;
+}
+
+export default function Home() {
+  const [surahs, setSurahs] = useState<Surah[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch(
+      'https://quran.ppqita.my.id/api/quran?listSurah=true&token=TADABBUR_EMAILKU'
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Gagal mengambil data');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setSurahs(data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-gray-900 text-gray-200 flex flex-col items-center p-6">
+      <h1 className="text-3xl font-bold mb-6">📖 Daftar Surah</h1>
+
+      {loading && <p className="text-gray-400">Loading...</p>}
+      {error && <p className="text-red-500">❌ {error}</p>}
+
+      {!loading && !error && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full max-w-6xl">
+          {surahs.map((surah) => (
+            <Link href={`surat/${surah.nomor}`} key={surah.nomor}>
+              <div className="bg-gray-800 p-4 rounded-lg shadow-lg hover:bg-gray-700 transition cursor-pointer">
+                <span className="text-xl font-bold block">{surah.nama}</span>
+                <span className="text-lg block">{surah.nama_latin}</span>
+                <div className="flex flex-row items-center gap-2 mt-2">
+                  <span className="text-md ">{surah.arti}</span>
+                  <span className="text-md text-gray-400">(Surat ke-{surah.nomor})</span>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
