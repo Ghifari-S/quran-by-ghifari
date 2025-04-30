a5f1aec5eb1611643178c9657ff541f4550ac90e
Hafidz <hafidzkarim18@gmail.com>
2025-04-30 10:21:33 +0700

    Tambah basmalah dan nama surat latin pada pencarian juz

--- src/app/juz/[juz_id]/components/QuranJuz.tsx
+++ src/app/juz/[juz_id]/components/QuranJuz.tsx
@@ -1,11 +1,11 @@
-'use client';
-import localFont from 'next/font/local';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import localFont from "next/font/local";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const lpmqFont = localFont({
-  src: '../../../../../font/LPMQ IsepMisbah.ttf',
-  variable: '--font-lpmq',
+  src: "../../../../../font/LPMQ IsepMisbah.ttf",
+  variable: "--font-lpmq",
 });
 
 const QuranJuz = ({ juz_id }: { juz_id: string }) => {
@@ -21,8 +21,9 @@ const QuranJuz = ({ juz_id }: { juz_id: string }) => {
         );
         const result = await response.json();
         setData(result);
+        console.log(result);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       } finally {
         setLoading(false);
       }
@@ -36,37 +37,55 @@ const QuranJuz = ({ juz_id }: { juz_id: string }) => {
 
   return (
     <div className="min-h-screen bg-gray-900 text-gray-200 flex flex-col items-center p-6">
-    {loading && <p className="text-gray-400">Loading...</p>}
-    {error && <p className="text-red-500">❌ {error}</p>}
+      {loading && <p className="text-gray-400">Loading...</p>}
+      {error && <p className="text-red-500">❌ {error}</p>}
 
-    {!loading && !error && data && (
-      <div className="w-full max-w-4xl">
-        <h1 className="text-3xl font-bold mb-2 text-center">
-          📖 Surah {data.data[0]?.surah.nama_latin}
-        </h1>
-        <p className="text-center text-gray-400 mb-6">
-          Total Ayat: {data.data[0]?.surah.jumlah_ayat}
-        </p>
+      {!loading && !error && data && (
+        <div className="w-full max-w-4xl">
+          {/* <h1 className="text-3xl font-bold mb-2 text-center">
+            📖 Surah {data.data[0]?.surah.nama_latin}
+          </h1>
+          <p className="text-center text-gray-400 mb-6">
+            Total Ayat: {data.data[0]?.surah.jumlah_ayat}
+          </p> */}
 
-        <div className="space-y-4">
-          {data.data.map((item: any) => (
-            <div
-              key={item.ayat.number.inQuran}
-              className="bg-gray-800 p-4 rounded-lg shadow-lg hover:bg-gray-700 transition"
-            >
-              <p className={`text-2xl text-right font-bold mb-2 ${lpmqFont.className}`}>
-                {item.ayat.arab}
-              </p>
-              <p className="text-sm italic text-gray-300 mb-1">{item.ayat.latin}</p>
-              <p className="text-base text-gray-200">{item.ayat.translation}</p>
-            </div>
-          ))}
+          <div className="space-y-4">
+            {data.data.map((item: any) => (
+              <div
+                key={item.ayat.number.inQuran}
+                className="bg-gray-800 p-4 rounded-lg shadow-lg hover:bg-gray-700 transition"
+              >
+                {item.ayat.number.inSurah == 1 && (
+                  <div className="m-8 text-center font-bold text-2xl">{item.surah.nama_latin}</div>
+                )}
+
+                {item.ayat.number.inSurah == 1 &&
+                  item.surah.nomor != 1 &&
+                  item.surah.nomor != 9 && (
+                    <div
+                      className={`${lpmqFont.className} flex justify-center`}
+                    >
+                      بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ
+                    </div>
+                  )}
+                <p
+                  className={`text-2xl text-right font-bold mb-2 ${lpmqFont.className}`}
+                >
+                  {item.ayat.arab}
+                </p>
+                <p className="text-sm italic text-gray-300 mb-1">
+                  {item.ayat.latin}
+                </p>
+                <p className="text-base text-gray-200">
+                  {item.ayat.number.inSurah}. {item.ayat.translation}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    )}
-  </div>
+      )}
+    </div>
   );
-
 };
 
 export default QuranJuz;
