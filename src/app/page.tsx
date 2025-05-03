4e175163115c6f123e955d527dda93ab800833e7
Ghifari-s <ghifaripooh@gmail.com>
2025-05-03 13:04:52 +0700

    mencoba menggunakan components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Navigation from "@/components/navigation-menu";
 import { useEffect, useState } from "react";
 import localFont from "next/font/local";
 
@@ -54,48 +55,7 @@ export default function Home() {
 
       {!loading && !error && (
         <div className="w-full">
-          <header className="w-full bg-gray-800 shadow-md mb-8">
-            <div className="max-w-6xl mx-auto flex items-center justify-between p-4">
-              {/* Logo atau Nama Aplikasi */}
-              <div className="text-2xl font-bold text-white">
-                📖 Tadabbur Quran
-              </div>
-
-              {/* Menu Navigasi */}
-              <nav className="flex space-x-6">
-                <Link
-                  href="/setoran/input-nama-santri"
-                  className="text-gray-300 hover:text-white transition"
-                >
-                  Tambah Santri
-                </Link>
-                <Link
-                  href="/solat"
-                  className="text-gray-300 hover:text-white transition"
-                >
-                  Jadwal Sholat
-                </Link>
-                <Link
-                  href="/juz"
-                  className="text-gray-300 hover:text-white transition"
-                >
-                  Pilih Juz
-                </Link>
-                <Link
-                  href="/pilih-surat"
-                  className="text-gray-300 hover:text-white transition"
-                >
-                  Pilih Surat
-                </Link>
-                <Link
-                  href="/tentang-kami"
-                  className="text-gray-300 hover:text-white transition"
-                >
-                  Tentang Kami
-                </Link>
-              </nav>
-            </div>
-          </header>
+          <Navigation/>
           <h1 className="text-3xl font-bold mb-6 text-center">
             📖 Daftar Surah
           </h1>
